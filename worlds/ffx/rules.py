80f5968b1b474ec6cb8ae584b86357bf8c264edc
Andrew K <andrewkilbourn1996@gmail.com>
2026-01-19 09:10:33 +1100

    list concatenation solution

--- worlds/ffx/rules.py
+++ worlds/ffx/rules.py
@@ -121,7 +121,7 @@ def create_min_summon_rule(world: FFXWorld, num_aeons: int) -> CollectionRule:
 
 def create_ranged_rule(world: FFXWorld) -> CollectionRule:
     return lambda state: (state.has_from_list_unique([f"Party Member: {name}" for name in ["Wakka", "Lulu"]], world.player, 1) or 
-                            (state.has(f"Party Member: Yuna", world.player) and state.has_from_list_unique([f"Party Member: {name}" for name in aeon_names[:6] + aeon_names[7]], world.player, 1))
+                            (state.has(f"Party Member: Yuna", world.player) and state.has_from_list_unique([f"Party Member: {name}" for name in aeon_names[:6] + aeon_names[7:]], world.player, 1))
                          )
 
 ruleDict: dict[str, Callable[[FFXWorld], CollectionRule]] = {
